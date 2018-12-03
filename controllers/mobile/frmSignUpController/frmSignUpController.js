6ec60a27b47a4d3c16328cad3418e720b034a3b3
Mihail Zhelev <mzhele@softserveinc.com>
2018-12-03 15:39:00 +0200

    uploading frmSigup

--- controllers/mobile/frmSignUpController/frmSignUpController.js
+++ controllers/mobile/frmSignUpController/frmSignUpController.js
@@ -1,5 +1,31 @@
 define({ 
+  Validate: function() {
+    var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
- //Type your controller code here 
+    if (this.view.txtUserName.text === null) {
+      this.view.txtUserName.text = '';
+    }
+    if (this.view.txtPass.text === null) {
+      this.view.txtPass.text = '';
+    }
 
- });
+    if (this.view.txtUserName.text.length < 1) {
+      this.view.lblInvalidMail.isVisible = true;
+      this.view.txtUserName.skin = "errSkin";
+    }
+
+    if (this.view.txtPass.text.length < 1) {
+      this.view.lblInvalidPass.isVisible = true;
+      this.view.txtPass.skin = "errSkin";
+    }
+    if (this.view.txtUserName.text.match && this.view.txtPass.text.length > 0) {
+      var ntf = new kony.mvc.Navigation("frmHome");
+      ntf.navigate();
+      this.view.txtUserName.text = '';
+      this.view.txtPass.text = '';
+      this.view.lblInvalidMail.isVisible = false;
+      this.view.lblInvalidPass.isVisible = false;
+      this.view.txtUserName.skin = "defTextBoxNormal";
+      this.view.txtPass.skin = "defTextBoxNormal";
+    }
+  }});
--- controllers/mobile/frmSignUpController/frmSignUpControllerActions.js
+++ controllers/mobile/frmSignUpController/frmSignUpControllerActions.js
@@ -1,7 +1,10 @@
-define({ 
-
- /* 
- This is an auto generated file and any modifications to it may result in corruption of the action sequence.
- */
-
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for Button0de6de2b9b23e40 **/
+    AS_Button_ee1c2d9dd9de4f8a8bdb70aa17d4b985: function AS_Button_ee1c2d9dd9de4f8a8bdb70aa17d4b985(eventobject) {
+        var self = this;
+        return self.Validate.call(this);
+    }
 });
