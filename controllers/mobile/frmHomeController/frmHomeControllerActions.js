cc4a06f0fbe364110291ca34d4ce02dd4dde7ace
Kristian Angelski <kange@softserveinc.com>
2018-12-04 21:58:33 +0200

    login form validation and navigation fixed

--- controllers/mobile/frmHomeController/frmHomeControllerActions.js
+++ controllers/mobile/frmHomeController/frmHomeControllerActions.js
@@ -2,19 +2,14 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
-    /** onClick defined for btnClose **/
-    AS_Button_i6a2575e4de5481e898d78a041e3b1ab: function AS_Button_i6a2575e4de5481e898d78a041e3b1ab(eventobject) {
-        var self = this;
-        return self.close.call(this);
-    },
     /** onClick defined for head **/
     AS_UWI_d759e0b468704407b51717667bd74006: function AS_UWI_d759e0b468704407b51717667bd74006(eventobject) {
         var self = this;
         return self.showAddFrm.call(this);
     },
-    /** onClick defined for btnTweet **/
-    AS_Button_gdb30ce584ef47efb6ee37fd76058f9f: function AS_Button_gdb30ce584ef47efb6ee37fd76058f9f(eventobject) {
+    /** onClick defined for btnClose **/
+    AS_Button_i6a2575e4de5481e898d78a041e3b1ab: function AS_Button_i6a2575e4de5481e898d78a041e3b1ab(eventobject) {
         var self = this;
-        return self.addPost.call(this);
+        return self.close.call(this);
     }
 });
--- controllers/mobile/frmLoginController/frmLoginController.js
+++ controllers/mobile/frmLoginController/frmLoginController.js
@@ -1,10 +1,40 @@
 define({ 
-	validateFields(field) {
-		return field.text !== null && field.text !== '';
-	},
-	
-	validateForm() {
-		
-	}
-	
+  validateField(field) {
+    return field.text !== null && field.text !== '';
+  },
+
+  validateForm() {
+    if (!this.validateField(this.view.txtEmail)) {
+      this.view.flxEmailHr.skin = 'sknLoginFrmInvalid';
+    } else {
+      this.view.flxEmailHr.skin = 'sknLoginFrmHR';
+    }
+
+    if (!this.validateField(this.view.txtPassword)) {
+      this.view.flxPasswordHr.skin = 'sknLoginFrmInvalid';
+    } else {
+      this.view.flxPasswordHr.skin = 'sknLoginFrmHR';
+    }
+
+    if ( this.validateField(this.view.txtEmail) && this.validateField(this.view.txtPassword) ) {
+      if (kony.string.isValidEmail(this.view.txtEmail.text)) {
+        this.logIn();
+      } else {
+        this.view.flxEmailHr.skin = 'sknLoginFrmInvalid';
+      }
+    }
+  },
+
+  logIn() {
+    let nav = new kony.mvc.Navigation('frmHome');
+    nav.navigate();
+  },
+
+  signUp() {
+    let nav = new kony.mvc.Navigation('frmSignUp');
+    nav.navigate();
+  }
+
+
+
 });
